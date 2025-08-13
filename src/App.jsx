42ddaa4582812fca861d64930c2ead8383edff7c
ikram000 <ikramjaved857@gmail.com>
2025-08-13 03:47:07 -0700

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 </div>
 <div className='bg-zinc-800 w-1/2 text-white m-auto p-1 pr-5 rounded-4xl border border-zinc-700 flex h-16'>
   <input type="text" className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
-  <button>ASK</button>
+  <button>ASK Anything</button>
 </div>
 </div>
    </div>
